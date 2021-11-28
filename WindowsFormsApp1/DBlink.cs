6eb8287087b465df8accb23a2bbe6f66c4ddc794
Dmitry <fedoseev.2001@list.ru>
2021-11-28 23:47:15 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/DBlink.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class DBlink
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=1234;database=tank_ft");
+
+        public MySqlConnection open_connection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+                Console.WriteLine("Подключено");
+            }
+            return connection;
+        }
+        public void close_connection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Dimcomission.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Dimcomission
+    {
+        private int comission_id;
+        private int tank_id;
+        private DateTime date_of_eval;
+        private bool is_accepted;
+        private string reason;
+        private MySqlDataReader data;
+        private List<string> data_o = new List<string>();
+
+
+        public void getStrings(MySqlConnection connection)
+        {
+            string sql = "SELECT * from dimcomission";
+            // объект для выполнения SQL-запроса
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            // выполняем запрос и получаем ответ
+            this.data = command.ExecuteReader();
+            this.data_o.Add(String.Format("{0,30}|{1,10}|{2,30}|{3,30}|{4,30}", "comission_id", "tank_id", "date_of_eval", "is_accepted", "reason"));
+        }
+
+        public List<string> editWindowRows()
+        {
+            while (this.data.Read()) // построчно считываем данные
+            {
+                this.comission_id = data.GetInt16(0);
+                this.tank_id = data.GetInt16(1);
+                this.date_of_eval = data.GetDateTime(2);
+                this.is_accepted = data.GetBoolean(3);
+                this.reason = data.GetString(4);
+
+                data_o.Add(String.Format("{0,30}|{1,10}|{2,30}|{3,30}|{4,30}", comission_id, tank_id, date_of_eval, is_accepted, reason));
+            }
+
+            return data_o;
+        }
+    }
+}
